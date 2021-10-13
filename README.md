0bae746cf6d064b7f68048d4e55618b962b49b9e
YUUKIToriyama <github@toriyama.dev>
2021-10-14 03:58:57 +0900

    コードに注釈を追加

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # traduk-masino
-Google Translation APIを試してみる
+
+Google Translation API を試してみる
+
+## 開発メモ
+
+### ダウンロードとセットアップ
+
+```bash
+# Clone this repo
+git clone https://github.com/YUUKIToriyama/traduk-masino.git
+# Install dependencies
+cd traduk-masino
+npm install
+```
+
+### 認証情報の設定方法
+
+1. Google Cloud Console で Translation API を有効化
+2. 認証情報を作成し、ダウンロードした json ファイルをリネームし`secret.json`に上書き
+3. プロジェクト ID を確認し、`src/main.js`の該当部分を修正
+4. `node src/main.js`を実行
--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
+// Translation APIにアクセスするためのライブラリを読み込みます。
 const { Translate } = require("@google-cloud/translate").v2;
 
+// 翻訳機を準備します。
 const translator = new Translate({
-	projectId: "",
-	keyFilename: "./secret.json"
+	projectId: "torichan-0129", // プロジェクトIDを指定します。
+	keyFilename: "./secret.json" // 秘密鍵の保存された場所を指定します。
 });
 
+// 任意のテキストを受け取って英語に変換する関数を作ります。
 const translateText = async (text) => {
-	const target = "en";
-	const result = await translator.translate(text, target);
-	console.log(result);
+	const target = "en"; // 何語に翻訳したいかを指定します。今回は英語を指定。
+	const result = await translator.translate(text, target); // translate関数で翻訳を実行。
+	console.log(result); // 結果をコンソールに表示します。
 };
 
-const textList = [
-	"こんにちは",
-	"私の名前は田中です。",
-	"好きな食べ物はハンバーグです。",
-	"どうぞよろしくお願いします。"
-];
-
-for (let text in textList) {
-	translateText(text);
-}
+// 実行例
+translateText("好きな食べ物はハンバーグです。");
